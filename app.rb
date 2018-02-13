92175fcd005f5118483c4b2ded7d6ed6f73541e2
Ben Rodenhäuser <ben@karo.berlin>
2018-02-13 13:56:07 +0100

    named parameters in url paths

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
-require './frankie'
+require './frankie2'
 
 get "/some_route" do
   @name = 'Franky'
   erb :hello
 end
 
-# get "/some_route/:some_id" do
-#   # handle pattern
-# end
+get "/some_route/:some_id" do
+  "matched the route"
+end
