6b2cfc8db116665eef0a5395c6f7071e1915a6e8
Ben Rodenhäuser <ben@karo.berlin>
2018-02-15 15:44:56 +0100

    cleanup

--- app.rb
+++ app.rb
@@ -13,9 +13,3 @@ end
 get "/segment/:some_id/segment/:some_other_id" do
   "Frankie has captured #{params['some_id']} and #{params['some_other_id']}."
 end
-
-post "/add_todo" do
-  @string = params[:submitted_string]
-  # store the string in some file?
-  redirect "/"
-end
