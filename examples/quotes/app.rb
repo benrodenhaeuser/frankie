411b82101e75c937f9bf3f673095fe86dd8423dd
Ben Rodenhäuser <ben@karo.berlin>
2018-02-21 14:37:55 +0100

    add tests for quotes app

--- examples/quotes/app.rb
+++ examples/quotes/app.rb
@@ -1,10 +1,22 @@
 require_relative '../../frankie'
+
+require 'erb'
 require 'yaml'
 
 use Rack::Session::Cookie, :key => 'rack.session', :secret => "secret"
 
 def data_file
-  File.expand_path('../data/quotes.yml', __FILE__)
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path('../test/data/quotes.yml', __FILE__)
+  else
+    File.expand_path('../data/quotes.yml', __FILE__)
+  end
+end
+
+def insert(author, quote, quotes)
+  next_id = quotes['next_id']
+  quotes[next_id] = { "author" => author, "quote" => quote }
+  quotes['next_id'] += 1
 end
 
 get "/" do
@@ -18,12 +30,6 @@ end
 
 get "/quotes/new" do
   erb :new_quote
-end
-
-def insert(author, quote, quotes)
-  next_id = quotes['next_id']
-  quotes[next_id] = { "author" => author, "quote" => quote }
-  quotes['next_id'] += 1
 end
 
 post "/quotes" do
--- examples/quotes/test/app_test.rb
+++ examples/quotes/test/app_test.rb
@@ -2,6 +2,7 @@ ENV["RACK_ENV"] = "test"
 
 require "minitest/autorun"
 require "rack/test"
+require 'erb'
 
 require_relative "../app"
 
@@ -12,8 +13,38 @@ class AppTest < Minitest::Test
     Frankie::Application
   end
 
-  def test_redirect_from_root
+  def test_get_root
     get "/"
     assert_equal 302, last_response.status
+    assert_equal "text/html", last_response["Content-Type"]
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'famously said'
+  end
+
+  def test_get_index
+    get "/quotes"
+    assert_equal 200, last_response.status
+    assert_equal "text/html", last_response["Content-Type"]
+    assert_includes last_response.body, 'famously said'
+  end
+
+  def test_get_new_quote
+    get "/quotes/new"
+    assert_equal 200, last_response.status
+    assert_equal "text/html", last_response["Content-Type"]
+    assert_includes last_response.body, 'Author'
+    assert_includes last_response.body, '<form'
+  end
+
+  def test_post_new_quote
+    post(
+      "/quotes",
+      { 'author' => 'Tom', 'quote' => 'Some nonsense.' }
+    )
+    assert_equal 303, last_response.status
+    get last_response['Location']
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, 'The quote has been added.'
   end
 end
